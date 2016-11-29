e0f320056f1ecf88a653c56cbcd15bfb8d247584
Sasha Sirotkin <sirotkin.sasha@gmail.com>
2016-11-29 00:10:57 -0500

    Better debugging, faster frame, reduced rollback

--- arduino-softpot-text-cursor/arduino-softpot-text-cursor.ino
+++ arduino-softpot-text-cursor/arduino-softpot-text-cursor.ino
@@ -6,7 +6,6 @@ const int BLUE_LED_PIN = 11;  // Blue LED Pin
 
 const int SENSOR_PIN = 0;      // Analog input pin
 
-
 void setup() {
   Serial.begin(SERIAL_HID_BAUD);
 }
@@ -15,7 +14,7 @@ void loop() {
   // Read the voltage from the softpot (0-1023)
   int sensorValue = analogRead(SENSOR_PIN);
   gestureCheck(sensorValue);
-  delay(50);
+  delay(20);
 }
 
 void sendKey(int key) {
@@ -39,16 +38,28 @@ void rightKey() {
   sendKey(KEY_RIGHT_ARROW);
 }
 
-void noKey() {
+void incomplete() {
+  analogWrite(RED_LED_PIN, 0);
+  analogWrite(GREEN_LED_PIN, 60);
+  analogWrite(BLUE_LED_PIN, 60);
+}
+
+void idle() {
+  analogWrite(RED_LED_PIN, 0);
+  analogWrite(GREEN_LED_PIN, 0);
+  analogWrite(BLUE_LED_PIN, 0);
+}
+
+void reset() {
   analogWrite(RED_LED_PIN, 0);
   analogWrite(GREEN_LED_PIN, 0);
   analogWrite(BLUE_LED_PIN, 255);
 }
 
 // The softpot is divided into 32 regions of 32 units each.
-const int STEP_SIZE = 32;
+const int STEP_SIZE = 16;
 // How many steps a motion need to reach to register as a swipe.
-const int STEPS_THRESHOLD = 1;
+const int STEPS_THRESHOLD = 2;
 // How many cycles of 0 voltage until the step count resets.
 const int IDLE_CYCLE_TOLERANCE = 2;
 
@@ -60,14 +71,16 @@ int gesture_steps = 0;
 int cycles_idle = 0;
 
 void gestureCheck(int value) {
-  if (value == 0) {
+  if (value <= 0) {
     // Given that the softpot can be finicky, we allow it to recieve a voltage of 0
     // for a few cycles before resetting the gesture.
-    if (cycles_idle == IDLE_CYCLE_TOLERANCE) {
+    if (cycles_idle == IDLE_CYCLE_TOLERANCE && gesture_start == -1) {
+      idle();
+    } else if (cycles_idle == IDLE_CYCLE_TOLERANCE) {
       cycles_idle = 0;
       gesture_start = -1;
       gesture_steps = 0;
-      noKey();
+      reset();
     } else {
       cycles_idle++;
     }
@@ -81,11 +94,12 @@ void gestureCheck(int value) {
   }
 
   // Add the distance travelled.
-  gesture_steps = (gesture_start - value) / STEP_SIZE;
+  gesture_steps = gesture_steps + ((gesture_start - value) / STEP_SIZE);
 
   // Do nothing if thresholds on either side are not reached.
   if (gesture_steps < STEPS_THRESHOLD && gesture_steps > -STEPS_THRESHOLD) {
-    noKey();
+    gesture_start = -1;
+    incomplete();
     return;
   }
 
@@ -102,7 +116,8 @@ void gestureCheck(int value) {
   }
 
   // Begin the next gesture
-  gesture_start = value;
+  gesture_start = -1;
   gesture_steps = 0;
 }
+
 
